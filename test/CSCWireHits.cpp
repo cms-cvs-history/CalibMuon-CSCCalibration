3f4eedca00ff02ee002588d7c8e94956bc85e036
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-02-23 15:29:44 +0000

    modified

--- test/CSCWireHits.cpp
+++ test/CSCWireHits.cpp
@@ -1,3 +1,5 @@
+//This will read AFEB data and create a root ntuple. 
+
 #include "IORawData/CSCCommissioning/src/FileReaderDDU.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCDDUEventData.h"
 #include "DataFormats/CSCDigi/interface/CSCWireDigi.h"
@@ -9,8 +11,8 @@
 #include "EventFilter/CSCRawToDigi/interface/CSCDDUTrailer.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCDMBHeader.h"
 
-#include "condbc.h"
-#include "cscmap.h" 
+#include "CalibMuon/CSCCalibration/interface/condbc.h"
+#include "CalibMuon/CSCCalibration/interface/cscmap.h" 
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -26,8 +28,6 @@
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TCanvas.h"
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TTree.h"
 
-using namespace std;
-
 class TCalibEvt { public:
   Int_t event;  
   Int_t chamber;
@@ -38,6 +38,9 @@ class TCalibEvt { public:
 
 
 int main(int argc, char **argv) {
+
+  //for debugging purposes from Alex Tumanov's code
+  //set to true if you wish to debug data
   CSCAnodeData::setDebug(false);
   CSCALCTHeader::setDebug(false);
   CSCCLCTData::setDebug(false); 
@@ -45,26 +48,26 @@ int main(int argc, char **argv) {
   CSCTMBData::setDebug(false);
   CSCDDUEventData::setDebug(false);
 
-
+  //root ntuple
   TCalibEvt calib_evt;
   TBranch *calibevt;
   TTree *calibtree;
   TFile *calibfile;
-  
- calibfile = new TFile("calibwire.root","RECREATE","Calibration Ntuple");
- calibtree = new TTree("Calibration","AFEB");
- calibevt = calibtree->Branch("EVENT", &calib_evt, "event/I:chamber/I:layer/I:wire/I:tbin/I");
- TH2F *h1 = new TH2F("h1","wire vs timebin",10,-1,8,10,0,80);
+    
+  calibfile = new TFile("calibwire.root","RECREATE","Calibration Ntuple");
+  calibtree = new TTree("Calibration","AFEB");
+  calibevt = calibtree->Branch("EVENT", &calib_evt, "event/I:chamber/I:layer/I:wire/I:tbin/I");
+  TH2F *h1 = new TH2F("h1","wire vs timebin",10,-1,8,10,0,80);
 
+  //data file: nr.of events,chambers read
   int maxEvents = 500000;
-  int mismatch = 0;
+  int misMatch = 0;
   std::string datafile=argv[1];
-  int dmbID=-999,crateID=-999,chamber_num,sector;
-  int i_chamber=0,ilayer=0;
+  std::string chamber_id; 
+  int dmbID=-999,crateID=-999,chamber_num,sector,ret_code;
   int reportedChambers =0;
-  int ret_code;
-  std::string chamber_id;
-  
+   
+  //needed for database and mapping
   condbc *cdb = new condbc(); 
   cscmap *map = new cscmap();
 
@@ -73,41 +76,17 @@ int main(int argc, char **argv) {
   FileReaderDDU ddu;
   ddu.open(datafile.c_str());
   
-  int evt=0;
-  std::vector<int> newadc;
-      
-  double a_pedestal[6][80];
-  double a_pedestal_sq[6][80];
-  double a_ped[6][80];
-  int a_dmbID[6][80];
-  int a_chamberID[6][80];
-  double new_ped[480];
-  double new_rms[480];
-  
-  for(int i=0;i<6;i++){
-    for (int j=0; j<80; j++){
-      a_pedestal[i][j] = 0.;
-      a_pedestal_sq[i][j] = 0.;
-      a_ped[i][j]=0.;
-      a_dmbID[i][j]=0;
-      a_chamberID[i][j]=0;
-    }
-  }
-  
-  for (int i=0;i<480;i++){
-    new_ped[i]=0;
-    new_rms[i]=0;
-  }
-  
+  //opening data files and checking data (copied from Alex Tumanov's code)  
   const unsigned short *dduBuf=0;
   int length = 1;
  
-  for (int event = 1; (event < maxEvents)&&(length); ++event) {
+  for (int event = 1; (event < maxEvents)&&(length); ++event) {//loop over all events in file (first one has empty DMB)
     std::cout << "---------- Event: " << event << "--------------" << std::endl;
+
     try {
       length= ddu.next(dduBuf);    
     } catch (std::runtime_error err ){
-      std::cout <<"digi Anal:: " << err.what()<<"  end of file?" << std::endl;
+      std::cout <<"AFEB calibration:: " << err.what()<<"  end of file?" << std::endl;
       break;
     }
     unsigned short * buf = (unsigned short *) dduBuf;
@@ -118,48 +97,57 @@ int main(int argc, char **argv) {
     int NChambers = cscData.size();
     int repChambers = dduEvent.header().ncsc();
     std::cout << " Reported Chambers = " << repChambers <<"   "<<NChambers<< std::endl;
-    if (NChambers!=repChambers) { std::cout<< "mismatched size!!!" << std::endl; mismatch++;}
+    if (NChambers!=repChambers) { std::cout<< "misMatched size!!!" << std::endl; misMatch++;}
 
-    evt++;
-
-    for (i_chamber=0; i_chamber<NChambers; i_chamber++) {
-      for(ilayer = 1; ilayer <= 6; ++ilayer) {
-	std::vector<CSCWireDigi> wire = cscData[i_chamber].wireDigis(ilayer) ;
+  
+    for (int i_chamber=0; i_chamber<NChambers; i_chamber++) {//loop over all DMBs
+      
+      for(int i_layer = 1; i_layer <= 6; ++i_layer) {//loop over all layers in chambers
+	
+	std::vector<CSCWireDigi> wire = cscData[i_chamber].wireDigis(i_layer) ;
 	const CSCDMBHeader &thisDMBheader = cscData[i_chamber].dmbHeader();
-	dmbID = cscData[i_chamber].dmbHeader().dmbID();
-	crateID = cscData[i_chamber].dmbHeader().crateID();
-	if(crateID == 255) continue;
-	for (int i=0; i < wire.size(); i++){
-	  int wire_group = wire[i].getWireGroup();
-	  int wire_tbin = wire[i].getBeamCrossingTag();
-
-	    calib_evt.event      = event;
-	    calib_evt.wire       = wire_group;
-	    calib_evt.tbin       = wire_tbin;
-	    calib_evt.chamber    = i_chamber;
-	    calib_evt.layer      = ilayer;
-	    h1->Fill(wire_group,wire_tbin);
- 
-	    calibtree->Fill();	    
-	    
-	}//wire.size
-      }//ilayer
-    }//chamber
-  }//event loop
+	
+	dmbID = cscData[i_chamber].dmbHeader().dmbID(); //get DMB ID
+	crateID = cscData[i_chamber].dmbHeader().crateID(); //get crate ID
+	if(crateID == 255) continue;//255 is reserved for old crate, present only 0 and 1
+	
+	for (unsigned int i=0; i < wire.size(); i++){ //loop over wire digis
+	  int wireGroup = wire[i].getWireGroup();
+	  int wireTBin = wire[i].getBeamCrossingTag();
+	  
+	  calib_evt.event      = event;
+	  calib_evt.wire       = wireGroup;
+	  calib_evt.tbin       = wireTBin;
+	  calib_evt.chamber    = i_chamber;
+	  calib_evt.layer      = i_layer;
+	  
+	  h1->Fill(wireGroup,wireTBin);
+	  
+	  calibtree->Fill();	    
+	  
+	}//end digis loop
+      }//end layer loop
+    }//end chamber loop
+  }//end event loop
   
-  
-  string test1="CSC_slice";
-  string test2="pedestal";
-  string test3="ped_rms";
-  
+    
+  //get chamber ID from Igor's mapping
   cout<<"Here is crate and DMB: "<<crateID<<"  "<<dmbID<<endl;
   map->crate_chamber(crateID,dmbID,&chamber_id,&chamber_num,&sector);
   cout<<"This is from mapping: "<< chamber_id<<"  "<<chamber_num<<"  "<<sector<<endl;
-
-  // To send infor to DB uncomment the next line(s)  
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, new_ped,2, &ret_code);
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, new_rms,2, &ret_code);
+  
+  //info needed for database
+  string test1="CSC_slice";
+  string test2="afeb";
+  string test3="wire";
+  
+  //*******to send this array to DB uncomment the next two line(s)*************
+  //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, smth_val1,1, &ret_code);
+  //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, smth_val2,1, &ret_code);
+  
+  //root ntuple end
   calibfile->Write();   
   calibfile->Close();
+
 }//main
 
