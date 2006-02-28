bce4b0e08317c1da680f3fe44e7dce340ff3825d
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-02-28 17:53:33 +0000

    modified to run on all present chambers

--- test/CSCCalibPedestalRMS.cpp
+++ test/CSCCalibPedestalRMS.cpp
@@ -1,6 +1,10 @@
-//This will create an array (480=whole ME2/3 chamber) of mean pedestal and RMS values
-//which can be sent to online database.
-//It creates a root ntuple for debugging purposes   
+/*
+  authors: Oana Boeriu, Nicole Ippolito
+  
+  This will create arrays (480=whole ME2/3 chamber) of pedestal and RMS values
+  which can be sent to online database.
+  It creates a root ntuple for debugging purposes.   
+*/
 
 #include "IORawData/CSCCommissioning/src/FileReaderDDU.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCDDUEventData.h"
@@ -27,6 +31,11 @@
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TH1.h"
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TCanvas.h"
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TTree.h"
+
+//constants declaration
+#define CHAMBERS 5
+#define LAYERS 6
+#define STRIPS 80
 
 class TCalibEvt { public:
   Int_t adc[8];
@@ -57,7 +66,7 @@ int main(int argc, char **argv) {
   TTree *calibtree;
   TFile *calibfile;
   
-  calibfile = new TFile("calibpedestal.root","RECREATE","Calibration Ntuple");
+  calibfile = new TFile("ntuples/calibpedestal.root","RECREATE","Calibration Ntuple");
   calibtree = new TTree("Calibration","Pedestal");
   calibevt = calibtree->Branch("EVENT", &calib_evt, "adc[8]/I:pedMean/F:strip/I:time[8]/F:chamber/I:event/I:layer/I");
   
@@ -66,7 +75,7 @@ int main(int argc, char **argv) {
   int misMatch = 0;
   std::string datafile=argv[1];
   std::string chamber_id;  
-  int dmbID=-999,crateID=-999,chamber_num,sector;
+  int dmbID[CHAMBERS],crateID[CHAMBERS],chamber_num,sector;
   int i_chamber=0,i_layer=0;
   int reportedChambers =0;
   int fff,ret_code;  
@@ -88,21 +97,24 @@ int main(int argc, char **argv) {
   int pedSum = 0, strip =-999;
   
   //definition of arrays
-  double arrayOfPed[6][80];
-  double arrayOfPedSquare[6][80];
-  double arrayPed[6][80];
+  double arrayOfPed[CHAMBERS][LAYERS][STRIPS];
+  double arrayOfPedSquare[CHAMBERS][LAYERS][STRIPS];
+  double arrayPed[CHAMBERS][LAYERS][STRIPS];
+
   double newPed[480];
   double newRMS[480];
   
   //initialize arrays
-  for(int i=0;i<6;i++){
-    for (int j=0; j<80; j++){
-      arrayOfPed[i][j] = 0.;
-      arrayOfPedSquare[i][j] = 0.;
-      arrayPed[i][j]=0.;
+  for(int i=0;i<CHAMBERS;i++){
+    for(int j=0; j<LAYERS; j++){
+      for(int k=0; k<STRIPS; k++){
+	arrayOfPed[i][j][k] = 0.;
+	arrayOfPedSquare[i][j][k] = 0.;
+	arrayPed[i][j][k]=0.;
+      }
     }
   }
-  
+
   for (int i=0;i<480;i++){
     newPed[i]=0;
     newRMS[i]=0;
@@ -143,40 +155,35 @@ int main(int argc, char **argv) {
 
 	if (thisDMBheader.cfebAvailable()){//check that CFEB data exists
 
-	  dmbID = cscData[i_chamber].dmbHeader().dmbID(); //get DMB ID
-	  crateID = cscData[i_chamber].dmbHeader().crateID(); //get crate ID
-	  if(crateID == 255) continue; //255 is reserved for old crate, present only 0 and 1
+	  dmbID[i_chamber]   = cscData[i_chamber].dmbHeader().dmbID(); //get DMB ID
+	  crateID[i_chamber] = cscData[i_chamber].dmbHeader().crateID(); //get crate ID
+	  if(crateID[i_chamber] == 255) continue; //255 is reserved for old crate, present only 0 and 1
 	  
 	  for (unsigned int i=0; i<digis.size(); i++){//loop over digis
 	    strip = digis[i].getStrip();
-	    adc = digis[i].getADCCounts();
+	    adc   = digis[i].getADCCounts();
 	    
-	    pedSum =adc[0]+adc[1];
+	    pedSum  = adc[0]+adc[1];
 	    pedMean = (float)pedSum/2.0;
 	    
 	    calib_evt.event    = event;
-	    calib_evt.pedMean = pedMean;
+	    calib_evt.pedMean  = pedMean;
 	    calib_evt.strip    = strip;
 	    calib_evt.chamber  = i_chamber;
 	    calib_evt.layer    = i_layer;
 
-	    int offset = event / 20;
-	    
-
 	    for(unsigned int k=0;k<adc.size();k++){//loop over timeBins
 	      
-	      time = (50*k)-((event)*6.25)+194+(200*offset);
-	          	      
-	      calib_evt.adc[k]      = adc[k];
-	      calib_evt.time[k]     = time;
-
+	      calib_evt.adc[k]  = adc[k];
+	      calib_evt.time[k] = time;
+	      
 	    }//adc.size
-
+	    
 	    calibtree->Fill();
-
-	    arrayPed[i_layer-1][strip-1] = pedMean;
-	    arrayOfPed[i_layer - 1][strip - 1] += pedMean;
-	    arrayOfPedSquare[i_layer - 1][strip - 1] += pedMean*pedMean ;
+	    
+	    arrayPed[i_chamber][i_layer-1][strip-1] = pedMean;
+	    arrayOfPed[i_chamber][i_layer - 1][strip - 1] += pedMean;
+	    arrayOfPedSquare[i_chamber][i_layer - 1][strip - 1] += pedMean*pedMean ;
 
 	  }//end digis loop
 	}//end if cfeb.available loop
@@ -184,41 +191,48 @@ int main(int argc, char **argv) {
     }//end chamber loop
   }//end events loop
   
-  //create array (480 entries) for database transfer
-  for (int i=0; i<6; i++){
-    for (int j=0; j<80; j++){
-      double meanPedestal = 0.;
-      fff = (i*80)+j;
-      double meanPedestalSquare = 0.;
-      double theRMS = 0.;
-      double thePedestal =0.;
-      double theRSquare = 0.;
+   //create array (480 entries) for database transfer
+  for(int myChamber=0; myChamber<CHAMBERS; myChamber++){
+    double meanPedestal = 0.;
+    double meanPedestalSquare = 0.;
+    double theRMS = 0.;
+    double thePedestal =0.;
+    double theRSquare = 0.;
+    string test1="CSC_slice";
+    string test2="pedestal";
+    string test3="ped_rms";
+    
+    for (int i=0; i<CHAMBERS; i++){
+      if (myChamber !=i) continue;
       
-      thePedestal = arrayPed[i][j];
-      meanPedestal = arrayOfPed[i][j] / evt;
-      newPed[fff] = meanPedestal;
-      meanPedestalSquare = arrayOfPedSquare[i][j] / evt;
-      theRMS = sqrt(abs(meanPedestalSquare - meanPedestal*meanPedestal));
-      newRMS[fff] = theRMS;
-      theRSquare = (thePedestal-meanPedestal)*(thePedestal-meanPedestal)/(theRMS*theRMS*theRMS*theRMS); 
+      for (int j=0; j<LAYERS; j++){
+	for (int k=0; k<STRIPS; k++){
+	  fff = (j*80)+k;
+	  thePedestal  = arrayPed[i][j][k];
+	  meanPedestal = arrayOfPed[i][j][k] / evt;
+	  newPed[fff]  = meanPedestal;
+	  meanPedestalSquare = arrayOfPedSquare[i][j][k] / evt;
+	  theRMS       = sqrt(abs(meanPedestalSquare - meanPedestal*meanPedestal));
+	  newRMS[fff]  = theRMS;
+	  theRSquare   = (thePedestal-meanPedestal)*(thePedestal-meanPedestal)/(theRMS*theRMS*theRMS*theRMS); 
+	  
+	  //std::cout <<" chamber "<<i<<" layer "<<j<<" strip "<<fff<<"  ped "<<newPed[fff]<<" RMS "<<newRMS[fff]<<std::endl;
+	}
+      }
     }
+    //get chamber ID from Igor's mapping
+
+    int new_crateID = crateID[myChamber];
+    int new_dmbID   = dmbID[myChamber];
+    std::cout<<"Here is crate: "<<new_crateID<<" and DMB:  "<<new_dmbID<<std::endl;
+    map->crate_chamber(new_crateID,new_dmbID,&chamber_id,&chamber_num,&sector);
+    std::cout<<"This is from mapping: "<< chamber_id<<"  "<<chamber_num<<"  "<<sector<<std::endl;
+
+    //write to database
+    //*******to send this array to DB uncomment the next two lines*************
+    //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, newPed,2, &ret_code);
+    //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, newRMS,2, &ret_code);
   }
-  
- 
-  //get chamber ID from Igor's mapping
-  cout<<"Here is crate and DMB: "<<crateID<<"  "<<dmbID<<endl;
-  map->crate_chamber(crateID,dmbID,&chamber_id,&chamber_num,&sector);
-  cout<<"This is from mapping: "<< chamber_id<<"  "<<chamber_num<<"  "<<sector<<endl;
-  
-  
-  //info needed for database
-  string test1="CSC_slice";
-  string test2="pedestal";
-  string test3="ped_rms";
-  
-  //*******to send this array to DB uncomment the next two lines*************
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, newPed,2, &ret_code);
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, newRMS,2, &ret_code);
   
   //root ntuple end
   calibfile->Write();   
