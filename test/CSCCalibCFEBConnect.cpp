38cb758fa1186702adf4d8ff3186a21c98f3b5ac
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2006-03-02 13:31:15 +0000

    modified to run on all present chambers

--- test/CSCCalibCFEBConnect.cpp
+++ test/CSCCalibCFEBConnect.cpp
@@ -1,4 +1,10 @@
-//Checks CFEB Connectivity
+/*
+
+  authors: Oana Boeriu, Nicole Ippolito
+  This will create arrays (480=whole ME2/3 chamber) of differences between max and min 
+  ADC values. Creates root ntuple for debugging.
+
+*/
 
 #include "IORawData/CSCCommissioning/src/FileReaderDDU.h"
 #include "EventFilter/CSCRawToDigi/interface/CSCDDUEventData.h"
@@ -25,6 +31,11 @@
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TH1.h"
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TCanvas.h"
 #include "/afs/cern.ch/cms/external/lcg/external/root/5.08.00/slc3_ia32_gcc323/root/include/TTree.h"
+
+//constants declaration
+#define CHAMBERS 5
+#define LAYERS 6
+#define STRIPS 80
 
 class TCalibEvt { public:
   Int_t adc[8];
@@ -54,7 +65,7 @@ int main(int argc, char **argv) {
   TTree *calibtree;
   TFile *calibfile;
 
-  calibfile = new TFile("ntuples/calibstripcon.root","RECREATE","Calibration Ntuple");
+  calibfile = new TFile("calibstripcon.root","RECREATE","Calibration Ntuple");
   calibtree = new TTree("Calibration","Connectivity");
   calibevt = calibtree->Branch("EVENT", &calib_evt, "adc[8]/I:strip/I:chamber/I:event/I:layer/I:adc_max[8]/I:adc_min[8]/I");
 
@@ -62,7 +73,7 @@ int main(int argc, char **argv) {
   int maxEvents = 500000;
   int misMatch = 0;
   std::string datafile=argv[1];
-  int dmbID=-999,crateID=-999,chamber_num,sector;
+  int dmbID[CHAMBERS],crateID[CHAMBERS],chamber_num,sector;
   int reportedChambers =0;
   int fff,ret_code,strip=-999;  
   std::string chamber_id;
@@ -81,29 +92,24 @@ int main(int argc, char **argv) {
   int evt=0;
       
   //definition of arrays
-  double a_diff[6][80];
-  double a_diff_sq[6][80];
   double new_diff[480];
-  double new_rms[480]; 
-  int adcMin[6][80], adcMax[6][80];
-  double diff[6][80];
+  int adcMin[CHAMBERS][LAYERS][STRIPS], adcMax[CHAMBERS][LAYERS][STRIPS];
+  double diff[CHAMBERS][LAYERS][STRIPS];
 
 
   //initialize arrays
-  for (int i=0; i<6; i++){
-    for (int j=0; j<80; j++){
-      a_diff[i][j] = 0.;
-      a_diff_sq[i][j] = 0.;
-      a_diff[i][j]=0.;
-      adcMin[i][j]=99999;
-      adcMax[i][j]=-99999;
-      diff[i][j]=0.0;
+  for (int i=0; i<CHAMBERS; i++){
+    for (int j=0; j<LAYERS; j++){
+      for (int k=0; k<STRIPS;k++){
+	adcMin[i][j][k]    = 9999999;
+	adcMax[i][j][k]    =-9999999;
+	diff[i][j][k]      = 0.;
+      }
     }
   }
 
   for (int i=0;i<480;i++){
-    new_diff[i]=0;
-    new_rms[i]=0;
+    new_diff[i]= 0;
   }
 
   //opening data files and checking data (copied from Alex Tumanov's code)
@@ -133,7 +139,7 @@ int main(int argc, char **argv) {
     if (NChambers!=repChambers) { std::cout<< "misMatched size!!!" << std::endl; misMatch++;}
 
     //
-    for (int i_chamber=0; i_chamber<1; i_chamber++) {//loop over all DMBs
+    for (int i_chamber=0; i_chamber<NChambers; i_chamber++) {//loop over all DMBs
 
       for(unsigned i_layer = 1; i_layer <=6; ++i_layer) {//loop over all layers in chambers
 
@@ -141,9 +147,9 @@ int main(int argc, char **argv) {
 	const CSCDMBHeader &thisDMBheader = cscData[i_chamber].dmbHeader();
 
 	if (thisDMBheader.cfebAvailable()){//check that CFEB data exists
-	  dmbID = cscData[i_chamber].dmbHeader().dmbID();//get DMB ID
-	  crateID = cscData[i_chamber].dmbHeader().crateID();//get crate ID
-	  if(crateID == 255) continue; //255 is reserved for old crate, present only 0 and 1
+	  dmbID[i_chamber] = cscData[i_chamber].dmbHeader().dmbID();//get DMB ID
+	  crateID[i_chamber] = cscData[i_chamber].dmbHeader().crateID();//get crate ID
+	  if(crateID[i_chamber] == 255) continue; //255 is reserved for old crate, present only 0 and 1
 	  
 	  for (unsigned int i=0; i<digis.size(); i++){//loop over digis
 	    std::vector<int> adc = digis[i].getADCCounts();
@@ -157,18 +163,17 @@ int main(int argc, char **argv) {
 	    for (unsigned int k=0;k<adc.size();k++){//loop over timeBins
 	      
 	      //find max ADC value
-	      if(adc[k] > adcMax[i_layer-1][strip-1]) {
-		adcMax[i_layer-1][strip-1]= adc[k];
+	      if(adc[k] > adcMax[i_chamber][i_layer-1][strip-1]) {
+		adcMax[i_chamber][i_layer-1][strip-1]= adc[k];
 	      }
 
 	      //find min ADC value
-	      if(adc[k] < adcMin[i_layer-1][strip-1]){
-		adcMin[i_layer-1][strip-1]=adc[k];
+	      if(adc[k] < adcMin[i_chamber][i_layer-1][strip-1]){
+		adcMin[i_chamber][i_layer-1][strip-1]=adc[k];
 	      }
 	      
-		calib_evt.adc_max[k]      = adcMax[i_layer-1][strip-1];
-		calib_evt.adc_min[k]      = adcMin[i_layer-1][strip-1];
-
+		calib_evt.adc_max[k] = adcMax[i_chamber][i_layer-1][strip-1];
+		calib_evt.adc_min[k] = adcMin[i_chamber][i_layer-1][strip-1];
 	    }//end timeBin loop
 
 	    calibtree->Fill();
@@ -178,46 +183,32 @@ int main(int argc, char **argv) {
     }//end chamber loop
   }//end events loop
 
-
-  for(int mylayer=0; mylayer<6;mylayer++){
-    for(int mystrip=0;mystrip<80;mystrip++){
-      diff[mylayer][mystrip]=adcMax[mylayer][mystrip]-adcMin[mylayer][mystrip];
+  for(int mychamber=0; mychamber<CHAMBERS;mychamber++){
+    for(int mylayer=0; mylayer<LAYERS;mylayer++){
+      for(int mystrip=0;mystrip<STRIPS;mystrip++){
+	diff[mychamber][mylayer][mystrip]=adcMax[mychamber][mylayer][mystrip]-adcMin[mychamber][mylayer][mystrip];
+	fff=(mylayer*80)+mystrip;
+	new_diff[fff] =diff[mychamber][mylayer][mystrip];
+	//std::cout<<"Chamber "<<mychamber<<" Layer "<<mylayer<<" Strip "<<mystrip<<" diff "<<new_diff[fff]<<std::endl; 
+      }
     }
+    //get chamber ID from Igor's mapping
+    int new_dmbID = dmbID[mychamber];
+    int new_crateID = crateID[mychamber];
+    cout<<"Here is crate and DMB: "<<new_crateID<<"  "<<new_dmbID<<endl;
+    map->crate_chamber(new_crateID,new_dmbID,&chamber_id,&chamber_num,&sector);
+    cout<<"This is from mapping: "<< chamber_id<<"  "<<chamber_num<<"  "<<sector<<endl;
+    
+    //info needed for database
+    string test1="CSC_slice";
+    string test2="cfeb_con_diff";
+    string test3="con_con_rms";
+    
+    //*******to send this array to DB uncomment the next two lines*************
+    //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, new_diff,1, &ret_code);
+    //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, new_rms, 1, &ret_code);
   }
-  
-  //create array (480 entries) for database transfer
-
-  for (int i=0; i<6; i++){ 
-    for (int j=0; j<80; j++){
-      fff = (i*80)+j;
-      double the_diff_sq = 0.;
-      double the_diff = 0.0,diff_rms=0.0;
-      int dmb_id =0;
-  
-      the_diff = a_diff[i][j]/evt;
-      the_diff_sq = a_diff_sq[i][j]/evt;
-      diff_rms = sqrt(abs(the_diff_sq - the_diff*the_diff));
-      new_diff[fff] = the_diff;
-      new_rms[fff] = diff_rms;
-      
-    }
-  }
-  
-  
-  //get chamber ID from Igor's mapping
-  cout<<"Here is crate and DMB: "<<crateID<<"  "<<dmbID<<endl;
-  map->crate_chamber(crateID,dmbID,&chamber_id,&chamber_num,&sector);
-  cout<<"This is from mapping: "<< chamber_id<<"  "<<chamber_num<<"  "<<sector<<endl;
-  
-  //info needed for database
-  string test1="CSC_slice";
-  string test2="cfeb_con_diff";
-  string test3="con_con_rms";
-
-  //*******to send this array to DB uncomment the next two lines*************
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test2,480, new_diff,1, &ret_code);
-  //cdb->cdb_write(test1,chamber_id,chamber_num,test3,480, new_rms, 1, &ret_code);
-
+    
   //root ntuple end
   calibfile->Write();
   calibfile->Close();
